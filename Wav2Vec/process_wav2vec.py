d7af1bd910c29f4f2dda6fc4af0f4361ccb08974
stella527 <salumona@terpmail.umd.edu>
2026-04-23 14:46:19 -0400

    Add files via upload

--- Wav2Vec/process_wav2vec.py
+++ Wav2Vec/process_wav2vec.py
@@ -1,4 +1,4 @@
-import start 
+import wav2vec_start 
 import os
 import pandas as pd
 
@@ -7,16 +7,16 @@ import torchaudio
 import soundfile as sf
 from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2Processor, Wav2Vec2ForCTC,  Wav2Vec2CTCTokenizer
 
-from start import shift_audio
-from start import process_shift
-from start import decoding_to_timings
+from wav2vec_start  import shift_audio
+from wav2vec_start  import process_shift
+from wav2vec_start  import decoding_to_timings
 
-audio_folder = start.audio_folder
-shift_ms = start.shift_range
+audio_folder = wav2vec_start.audio_folder
+shift_ms = wav2vec_start.shift_range
 shifted_dfs = {}
 
 
-output_folder = start.output_folder
+output_folder = wav2vec_start.output_folder
 
 
 for wav in os.listdir(audio_folder):
@@ -51,8 +51,8 @@ for wav in os.listdir(audio_folder):
                 ms,
                 sr,
                 output_folder,
-                start.processor,
-                start.model,
+                wav2vec_start.processor,
+                wav2vec_start.model,
                 decoding_to_timings)
 
             shifted_df.to_csv(
@@ -66,42 +66,3 @@ for wav in os.listdir(audio_folder):
 
 dfs = []
 
-for file in os.listdir(output_folder):
-    if file.endswith(".csv") and file != "Wav2vec_OG.csv" and not file.startswith("combined"):
-        #Get path of each file
-        file_path = os.path.join(output_folder, file)
-       
-        # Read CSV
-        df = pd.read_csv(file_path)
-
-        #Get the name without the extension
-        base = os.path.splitext(file)[0]
-
-        #Split each word in the name
-        parts = base.split("_")
-        #print(parts)
-
-        # The timing value is in index 2
-        timing = parts[2]   
-    
-         # Rename columns to timing_columnname
-        df.columns = [f"{timing}_{col}" for col in df.columns]
-
-        # Store
-        dfs.append(df)
-
-#All the shifted csv appending
-combined_df = pd.concat(dfs, axis=1)
-
-# Read the non-shifted
-df_wav2vec = pd.read_csv(f"{output_folder}/Wav2Vec_OG.csv")
-
-# Reset index
-df_wav2vec.reset_index(drop=True, inplace=True)
-
-#combine them into one
-full_df = pd.concat([combined_df, df_wav2vec], axis = 1)
-
-full_df.to_csv(os.path.join(output_folder, "combined.csv"), index=False)
-
-print("Done:", full_df.shape)
